836faa8900635c436e090229b6f5ae1ff38ce248
kdeoliveira <contact@kevindeoliveira.com>
2020-02-21 16:26:00 -0500

    third commit

--- src/player.py
+++ src/player.py
@@ -8,19 +8,23 @@ class Player:
         self.velocity = 5
         self.isJump = False
         self.jumpctn = 10
-        self.left = False
+        self.left = True
         self.right = False
         self.walkCount = 0
+        self.hitbox = (self.x + 20, self.y + 10, 23, 51)
+
+
         self.walkRight = [pygame.image.load('img/R1.png'), pygame.image.load('img/R2.png'), pygame.image.load('img/R3.png'), pygame.image.load('img/R4.png'), pygame.image.load('img/R5.png'), pygame.image.load('img/R6.png'), pygame.image.load('img/R7.png'), pygame.image.load('img/R8.png'), pygame.image.load('img/R9.png')]
         self.walkLeft = [pygame.image.load('img/L1.png'), pygame.image.load('img/L2.png'), pygame.image.load('img/L3.png'), pygame.image.load('img/L4.png'), pygame.image.load('img/L5.png'), pygame.image.load('img/L6.png'), pygame.image.load('img/L7.png'), pygame.image.load('img/L8.png'), pygame.image.load('img/L9.png')]
         self.char = pygame.image.load('img/standing.png')
 
 
-    def draw(self, win):
+    def draw(self, win):            
 
         # Drawing shapes on window
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
+
         if self.left:
             win.blit(self.walkLeft[self.walkCount//3], (self.x, self.y))
             self.walkCount += 1
@@ -29,3 +33,73 @@ class Player:
             self.walkCount += 1
         else:
             win.blit(self.char, (self.x, self.y))
+        
+        #Debugging only - hitbox
+        self.hitbox = (self.x + 20, self.y + 10, 23, 51)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+
+
+# PROJECTILE CLASS FOR BULLETS
+class Projectile:
+    def __init__(self,x , y, radius, color, direction):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.direction = direction
+        self.velocity = 8 * direction
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
+class Enemies:
+    walkRight = [pygame.image.load('img/R1E.png'), pygame.image.load('img/R2E.png'), pygame.image.load('img/R3E.png'), pygame.image.load('img/R4E.png'), pygame.image.load('img/R5E.png'), pygame.image.load('img/R6E.png'), pygame.image.load('img/R7E.png'), pygame.image.load('img/R8E.png'), pygame.image.load('img/R9E.png'),pygame.image.load('img/R10E.png'),pygame.image.load('img/R11E.png')]
+    walkLeft = [pygame.image.load('img/L1E.png'), pygame.image.load('img/L2E.png'), pygame.image.load('img/L3E.png'), pygame.image.load('img/L4E.png'), pygame.image.load('img/L5E.png'), pygame.image.load('img/L6E.png'), pygame.image.load('img/L7E.png'), pygame.image.load('img/L8E.png'), pygame.image.load('img/L9E.png'),pygame.image.load('img/L10E.png'),pygame.image.load('img/L11E.png')]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walkCount = 0
+        self.velocity = 3
+        self.hitbox = (self.x + 15, self.y, 25, 60)
+
+    def draw(self, win):
+        self.move()
+        if self.walkCount + 1 >= 33:
+            self.walkCount = 0
+        
+        if self.velocity > 0:
+            win.blit(self.walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(self.walkLeft[self.walkCount//3], (self.x - 10, self.y))
+            self.walkCount += 1
+
+        self.hitbox = (self.x + 15, self.y, 25, 60)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        pass
+
+    def move(self):
+        if self.velocity > 0:
+                if self.x + self.velocity < self.path[1]:
+                    self.x += self.velocity
+                else:
+                    self.velocity *= -1
+                    self.walkCount = 0
+        else:
+            if self.x - self.velocity > self.path[0]:
+                self.x += self.velocity
+            else:
+                self.velocity *= -1
+                self.walkCount = 0
+        pass
+    
+    def hit(self):
+        print("Enemie hit")
+        pass
--- src/window.py
+++ src/window.py
@@ -10,27 +10,65 @@ bg = pygame.image.load('img/bg.jpg')
 clock = pygame.time.Clock()
 
 man = Player(300, 418, 64, 64)
+enemy = Enemies(100, 418, 64, 64, 450)
+bullets = []
 
 def redraw():
     # Fills background color
     win.blit(bg, (0,0))
+    #Drawing elements
     man.draw(win)
+    for bullet in bullets:
+        bullet.draw(win)
+    enemy.draw(win)
     pygame.display.update()
 
+
+shoot = 0
 run = True
 # main loop
 while run:
     # clock for game (refresh/frame rate)
     clock.tick(27)
 
+    #Manual timer for shooting
+    if shoot > 2:
+        shoot = 0
+    else:
+        shoot += 1
 
     # Event -> any input change
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    for bullet in bullets:
+        #Checking for collisions
+        if bullet.y - bullet.radius < enemy.hitbox[1] + enemy.hitbox[3] and bullet.y + bullet.radius > enemy.hitbox[1]:
+            if bullet.x + bullet.radius > enemy.hitbox[0] and bullet.x - bullet.radius < enemy.hitbox[0] + enemy.hitbox[2]:
+                enemy.hit()
+                bullets.pop(bullets.index(bullet))
+
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.velocity
+        else:
+            bullets.pop(bullets.index(bullet))
+
+
     keys = pygame.key.get_pressed()
     
+    #Shooting
+    if keys[pygame.K_SPACE] and not(shoot):
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(Projectile(
+                round(man.x + man.width//2), 
+                round(man.y + man.height//2), 6, (0,0,0), facing))
+        shoot = 1
+
     if keys[pygame.K_LEFT] and man.x > man.velocity:
         man.x -= man.velocity
         man.left = True
@@ -40,9 +78,14 @@ while run:
         man.left = False
         man.right = True
     else:
-        man.right = False
-        man.left = False
         man.walkCount = 0
+        if man.right:
+            man.left = False
+        elif man.left:
+            man.right = False
+        else:    
+            man.right = False
+            man.left = False
 
 
     if not(man.isJump): 
